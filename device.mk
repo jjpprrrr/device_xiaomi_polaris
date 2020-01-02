4ad19a6716fff07f2a259f08314055a272698be9
YumeMichi <do4suki@gmail.com>
2020-01-02 12:10:43 +0800

    polaris: Enable double tap to wake feature
    Change-Id: I3d9de7d4e40d550459138dec5ba1bb5c68cb2b8e
    Signed-off-by: YumeMichi <do4suki@gmail.com>
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- device.mk
+++ device.mk
@@ -18,7 +18,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay 
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-pa
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage/lineage-sdk
