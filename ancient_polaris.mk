0993f9e9b12dc82d5602c33d727cbb2e0fddb178
Chenyang Zhong <zhongcy95@gmail.com>
2020-04-02 01:56:06 -0400

    polaris: initial Ancient bringup

--- ancient_polaris.mk
+++ ancient_polaris.mk
@@ -6,16 +6,16 @@
 
 $(call inherit-product, device/xiaomi/polaris/device.mk)
 
-# Inherit some common PixelExperience stuff.
+# Inherit some common Ancient stuff.
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_WIFI_EXT := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_STOCK_ARCORE := true
 
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_polaris
+PRODUCT_NAME := ancient_polaris
 PRODUCT_DEVICE := polaris
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi MIX 2S
@@ -28,3 +28,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="polaris"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Ancient stuff
+export ANCIENT_BUILDTYPE=Unofficial
+export ANCIENT_DEVELOPER=jjpprrrr
