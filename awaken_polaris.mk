254700014089453c71ffeb1bcc13e10077b7527d
Chenyang Zhong <zhongcy95@gmail.com>
2020-09-25 12:51:08 -0400

    polaris: use fingerprint from crosshatch R September build
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- awaken_polaris.mk
+++ awaken_polaris.mk
@@ -21,10 +21,10 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi MIX 2S
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "Xiaomi/polaris/polaris:8.0.0/OPR1.170623.032/V9.5.19.0.ODGMIFA:user/release-keys"
+BUILD_FINGERPRINT := "google/crosshatch/crosshatch:11/RP1A.200720.009/6720564:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="polaris-user 8.0.0 OPR1.170623.032 V9.5.19.0.ODGMIFA release-keys" \
+    PRIVATE_BUILD_DESC="crosshatch-user 11 RP1A.200720.009 6720564 release-keys" \
     PRODUCT_NAME="polaris"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
