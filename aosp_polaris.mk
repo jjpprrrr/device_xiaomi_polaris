451b4f305cd06ab43a7e26367ed2ccd5ead0b92f
Chenyang Zhong <zhongcy95@gmail.com>
2021-10-08 02:38:01 -0400

    Revert "polaris: use AOSP recovery"
    This reverts commit 3f104339493f428dc1827bfa986958f51ed2e121.

--- aosp_polaris.mk
+++ aosp_polaris.mk
@@ -11,7 +11,6 @@ TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_STOCK_ARCORE := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
-TARGET_USES_AOSP_RECOVERY := true
 
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
