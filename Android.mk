cfdff3cbaba5f8bf1502db0f4ad8eb1f1c231952
Chenyang Zhong <zhongcy95@gmail.com>
2021-10-08 02:37:41 -0400

    poalris: KERNEL_OBJ/usr
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- Android.mk
+++ Android.mk
@@ -18,3 +18,9 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(TARGET_DEVICE),polaris)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
+
+# copy kernel headers to the build tree
+$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr: $(wildcard $(PRODUCT_VENDOR_KERNEL_HEADERS)/*)
+	rm -rf $@
+	mkdir -p $@/include
+	cp -a $(PRODUCT_VENDOR_KERNEL_HEADERS)/. $@/include
