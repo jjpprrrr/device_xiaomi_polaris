b99b82a0e2038d1be42a28d74708ddd393cc928f
Chenyang Zhong <zhongcy95@gmail.com>
2021-11-21 02:27:49 -0500

    polaris: remove more packages
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := YouTube YouTubeMusicPrebuilt
+LOCAL_OVERRIDES_PACKAGES := YouTube YouTubeMusicPrebuilt Drive Maps Tycho PrebuiltGmail CarrierSetup MyVerizonServices SprintDM SprintHM VZWAPNLib
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
