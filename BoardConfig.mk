ef139389e6ca8bdc8722c8a5c44d988fb001ef01
Chenyang Zhong <zhongcy95@gmail.com>
2022-11-21 08:47:10 -0500

    Revert "Revert "polaris: drop the smart charging sepolicy for now""
    This reverts commit 64ad5986f205c3e97f4eaf2594e5165b6bda670a.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -37,7 +37,7 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
 # Sepolicy
-BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+#BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Inherit from the proprietary version
 include vendor/xiaomi/polaris/BoardConfigVendor.mk
